5de1f598128c48b1d7e60eba5262af6bd2ddc911
Marcel Riera <mrier011@fiu.edu>
2020-09-06 10:32:20 -0400

    First commit - empty project

---
+++ src/main/java/com/uhmarcel/scrumtaskmanager/ScrumTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.uhmarcel.scrumtaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uhmarcel/scrumtaskmanager/ScrumTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhmarcel.scrumtaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
