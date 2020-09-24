ac084aed8be7de6b1bc849d2b5e1b805897aa858
uhmarcel <mrier011@fiu.edu>
2020-09-24 00:00:30 -0400

    Increased page size to 1000

--- src/main/java/com/uhmarcel/storytasks/configuration/ControllerConstants.java
+++ src/main/java/com/uhmarcel/storytasks/configuration/ControllerConstants.java
@@ -4,7 +4,7 @@ public final class ControllerConstants {
 
     public static final String APP_ROUTE_PREFIX = "/api/v1";
 
-    public static final int PAGE_SIZE_DEFAULT = 10;
+    public static final int PAGE_SIZE_DEFAULT = 1000;
     public static final long NO_PARENT_ID = -1;
 
     private ControllerConstants() {}
